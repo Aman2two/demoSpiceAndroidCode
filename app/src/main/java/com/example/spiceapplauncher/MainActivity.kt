6993776e1700447f93d476cf2d16ff2afcde2a72
aman.kumar <aman.kumar@decimal.co.in>
2021-04-10 03:05:45 +0530

    code working fine

--- app/src/main/java/com/example/spiceapplauncher/MainActivity.kt
+++ app/src/main/java/com/example/spiceapplauncher/MainActivity.kt
@@ -3,18 +3,22 @@ package com.example.spiceapplauncher
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity;
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var button=findViewById<Button>(R.id.btn_open_app)
+        val button=findViewById<Button>(R.id.btn_open_app)
         button.setOnClickListener {
             // launch flutter app
-            startActivity(
-                FlutterActivity.createDefaultIntent(this)
-            );
+            try {
+                startActivity(FlutterActivity.createDefaultIntent(this));
+            }catch (e:Exception){
+                Toast.makeText(this,e.message,Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
