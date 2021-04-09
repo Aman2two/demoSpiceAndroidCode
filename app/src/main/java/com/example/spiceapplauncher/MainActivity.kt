25e9cce488288a60c76466d3c74a511634ed257d
aman.kumar <aman.kumar@decimal.co.in>
2021-04-10 02:49:05 +0530

    flutter app integrated

--- app/src/main/java/com/example/spiceapplauncher/MainActivity.kt
+++ app/src/main/java/com/example/spiceapplauncher/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.spiceapplauncher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button=findViewById<Button>(R.id.btn_open_app)
+        button.setOnClickListener {
+            // launch flutter app
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            );
+        }
     }
 }
